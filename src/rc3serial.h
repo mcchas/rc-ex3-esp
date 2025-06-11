359f0956b34be526fe2a31752719c1094a1e2cfa
mcchas <user@local>
2025-06-12 00:10:11 +1000

    work in progress

--- src/rc3serial.h
+++ src/rc3serial.h
@@ -7,3 +7,10 @@ void setPowerOn(uint8_t state);
 void setTemp(uint16_t degrees);
 void setMode(uint8_t mode);
 void getStatus();
+typedef struct
+{
+  bool power;
+  uint8_t mode;
+  uint16_t degrees;
+  uint8_t speed;
+} Settings;
